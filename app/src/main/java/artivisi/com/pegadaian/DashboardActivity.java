17e191fbff4a29ff6f69722e4e34e70e72590556
tedimulyadi <tedimulyadi92@gmail.com>
2016-04-20 14:49:30 +0700

    dashboard ui

--- app/src/main/java/artivisi/com/pegadaian/DashboardActivity.java
+++ app/src/main/java/artivisi/com/pegadaian/DashboardActivity.java
@@ -3,23 +3,14 @@ package artivisi.com.pegadaian;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by teddy on 20/04/16.
  */
-public class LoginActivity extends AppCompatActivity{
-
-    Button btnLogin;
+public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-
-    public void btnLogin(View view){
-        startActivity(new Intent("artivisi.com.DashboardActivity"));
+        setContentView(R.layout.activity_dashboard);
     }
 }
